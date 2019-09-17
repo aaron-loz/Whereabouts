3c129a44e2e33534604c47e4f0d02484709a95ea
Clara Fairbanks <clarafairbanks@claras-mbp.home>
2019-09-17 17:23:39 -0400

    Created component structure: added blank SplashScreen; LoginScreen; HomeScreen; MapScreen; MapListScreen; FriendsListScreen; and LikesListScreen Components.  Implemented navigation structure using react-navigation

--- whereabouts-app/App.js
+++ whereabouts-app/App.js
@@ -1,13 +1,130 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+
+
+class SplashScreen extends React.Component  {
+
+  static navigationOptions = {
+    title: 'Splash Screen',
+  };
+  render() {
+    return (
+      <View style= {styles.container} >
+        <Text>Splish Splash!</Text>
+      </View>
+    );
+  }
+};
+
+class LoginScreen extends React.Component  {
+
+  static navigationOptions = {
+    title: 'Login Screen',
+  };
+  render() {
+    return (
+      <View style= {styles.container} >
+        <Text>Welcome!</Text>
+      </View>
+    );
+  }
+};
+
+class HomeScreen extends React.Component {
+
+  static navigationOptions = {
+    title: 'Home Screen',
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Home Screen</Text>
+      </View>
+    );
+  }
+};
+
+
+class MapScreen extends React.Component  {
+  static navigationOptions = {
+    title: 'Map Screen',
+  };
+  render() {
+    return (
+      <View style= {styles.container}>
+        <Text>This is a Map!</Text>
+      </View>
+    );
+  }
+};
+
+class MapListScreen extends React.Component  {
+  static navigationOptions = {
+    title: 'Map Screen',
+  };
+  render() {
+    return (
+      <View style= {styles.container}>
+        <Text>List of Map Pin Data</Text>
+      </View>
+    );
+  }
+};
+
+class FriendsListScreen extends React.Component  {
+  static navigationOptions = {
+    title: 'Friends List Screen',
+  };
+  render() {
+    return (
+      <View style= {styles.container}>
+        <Text>Here are all of your Friends!</Text>
+      </View>
+    );
+  }
+};
+
+class LikesListScreen extends React.Component  {
+  static navigationOptions = {
+    title: 'Likes List Screen',
+  };
+  render() {
+    return (
+      <View style= {styles.container}>
+        <Text>Here are all of your Likes!</Text>
+      </View>
+    );
+  }
+};
+
+const AppNavigator = createStackNavigator({
+  Splash: {
+    screen: SplashScreen,
+  },
+  Login: {
+    screen: LoginScreen,
+  },
+  Home: {
+    screen: HomeScreen,
+  },
+  Map: {
+    screen: MapScreen,
+  },
+  MapList: {
+    screen: MapListScreen,
+  },
+  Friends: {
+    screen: FriendsListScreen,
+  },
+  Likes: {
+    screen: LikesListScreen,
+  },
+});
+
+export default createAppContainer(AppNavigator);
 
 const styles = StyleSheet.create({
   container: {

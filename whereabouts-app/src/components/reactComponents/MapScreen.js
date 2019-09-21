7a1d4afd19071ec058ac602d7ae3f1d9cb865a2c
Liudmila Zyrianova <liudmila.zyrianova@gmail.com>
2019-09-21 14:39:58 -0400

    Initialized FB

--- whereabouts-app/src/components/reactComponents/MapScreen.js
+++ whereabouts-app/src/components/reactComponents/MapScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-import ItemComponent from './ItemComponent';
 import { db } from '../../config/firebaseConfig';
-let itemsRef = db.ref('/items');
+let accountIdsRef = db.ref('/accountIds');
+let friedsIdsRef = db.ref('/friends');
+let likesIdsRef = db.ref('/likes');
 import styles from './styles';
 
 export default class MapScreen extends React.Component  {
@@ -11,28 +12,104 @@ export default class MapScreen extends React.Component  {
     };
   
     state = {
-      items: []
+      accounts: [],
+      friends: [],
+      likes: []
     };
   
     componentDidMount() {
-      itemsRef.on('value', snapshot => {
+      accountIdsRef.on('value', snapshot => {
         let data = snapshot.val();
-        let items = Object.values(data);
-        this.setState({ items });
+        let accounts = Object.values(data);
+        this.setState({ accounts });
+      });
+      friedsIdsRef.on('value', snapshot => {
+        let data = snapshot.val();
+        let friends = Object.values(data);
+        this.setState({ friends });
+      });
+      likesIdsRef.on('value', snapshot => {
+        let data = snapshot.val();
+        let likes = Object.values(data);
+        this.setState({ likes });
       });
     }
   
     render() {
       return (
-        <View style= {styles.container}>
+        <View style= {styles.main}>
+          <Text style={styles.title}>This is a Map!</Text>
+
+          <Text>--------------------</Text>
+
+          {/* Renders accounts */}
+          <Text style={styles.title}>Accounts:</Text>
           <Text>
-            {this.state.items.length > 0 ? (
-              <ItemComponent items={this.state.items} />
+            {this.state.accounts.length > 0 ? (
+              <Text>
+                {this.state.accounts.map((acc, index) => {
+                  return (
+                    <Text key={index}>{acc.id}   </Text>
+                  );
+                })}
+              </Text>
             ) : (
-              <Text>No items</Text>
+              <Text>No accounts</Text>
             )}
           </Text>
-          <Text>This is a Map!</Text>
+
+          <Text>--------------------</Text>
+
+          {/* Renders friends */}
+          <Text style={styles.title}>Friends:</Text>
+          <Text>
+            {this.state.friends.length > 0 ? (
+              <Text>
+                {this.state.friends.map((acc, index) => {
+                    return (
+                      <Text key={index}>
+                        {Object.values(acc).map((friend, index1) => {
+                          return(
+                            <Text key={index1}>{friend.friendId}  </Text>
+                          )
+                        })}
+                        <Text>...</Text>
+                      </Text>
+                    );
+                })}
+              </Text>
+            ) : (
+              <Text>No friends</Text>
+            )}
+          </Text>
+
+          <Text>--------------------</Text>
+
+          {/* Renders likes */}
+          <Text style={styles.title}>Likes:</Text>
+          <Text>
+            {this.state.likes.length > 0 ? (
+              <Text>
+                {this.state.likes.map((acc, index) => {
+                    return (
+                      <Text key={index}>
+                        {Object.values(acc).map((like, index1) => {
+                          return(
+                            <Text key={index1}>{like.locationId}  </Text>
+                          )
+                        })}
+                        <Text>... </Text>
+                      </Text>
+                    );
+                })}
+              </Text>
+            ) : (
+              <Text>No likes</Text>
+            )}
+          </Text>
+
+          <Text>--------------------</Text>
+
         </View>
       );
     }
--- whereabouts-app/src/components/reactComponents/styles.js
+++ whereabouts-app/src/components/reactComponents/styles.js
@@ -7,4 +7,44 @@ export default styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
+    //This is a temporary stuff:
+    main: {
+      flex: 1,
+      padding: 30,
+      flexDirection: 'column',
+      justifyContent: 'center',
+      backgroundColor: '#6565fc'
+    },
+    title: {
+      marginBottom: 20,
+      fontSize: 20,
+      textAlign: 'center'
+    },
+    itemInput: {
+      height: 40,
+      padding: 4,
+      marginRight: 5,
+      fontSize: 17,
+      borderWidth: 1,
+      borderColor: 'white',
+      borderRadius: 8,
+      color: 'white'
+    },
+    buttonText: {
+      fontSize: 15,
+      color: '#111',
+      alignSelf: 'center'
+    },
+    button: {
+      height: 35,
+      flexDirection: 'row',
+      backgroundColor: 'white',
+      borderColor: 'white',
+      borderWidth: 1,
+      borderRadius: 8,
+      marginBottom: 10,
+      marginTop: 10,
+      alignSelf: 'stretch',
+      justifyContent: 'center'
+    }
   });

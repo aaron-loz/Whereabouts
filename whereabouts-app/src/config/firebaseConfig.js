2e6d47cdf140ce1e7db71ab1143e16247e9047cb
Liudmila Zyrianova <liudmila.zyrianova@gmail.com>
2019-10-02 16:45:20 -0400

    Handled env

--- whereabouts-app/src/config/firebaseConfig.js
+++ whereabouts-app/src/config/firebaseConfig.js
@@ -1,14 +1,16 @@
 import Firebase from 'firebase';
+import Config from '../../config';
 
 let config = {
-    apiKey: process.env.FB_KEY,
-    authDomain: process.env.FB_AUTH_DOMAIN,
-    databaseURL: "https://whereabouts-c71c4.firebaseio.com",
-    projectId: process.env.FB_PROJECT_ID,
+    apiKey: Config.FB_KEY,
+    authDomain: Config.FB_AUTH_DOMAIN,
+    databaseURL: Config.FB_URL,
+    projectId: Config.FB_PROJECT_ID,
     storageBucket: "",
-    messagingSenderId: process.env.FB_SENDER_ID,
-    appId: process.env.FB_APP_ID
+    messagingSenderId: Config.FB_SENDER_ID,
+    appId: Config.FB_APP_ID
 };
+
 
 let app = Firebase.initializeApp(config);
 export const db = app.database();

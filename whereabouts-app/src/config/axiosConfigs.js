c7e5780be1ca65ab34756e6c0926611520f6789f
Aaron Lopez <dazdndcunfusd@gmail.com>
2019-10-09 21:50:10 -0400

    refactored node.js' randomBytes for react native. created nonce

--- whereabouts-app/src/config/axiosConfigs.js
+++ whereabouts-app/src/config/axiosConfigs.js
@@ -2,13 +2,19 @@ import axios from 'react-native-axios'
 import {encode as btoa} from 'base-64'
 import { Actions } from 'react-native-router-flux';
 import config from '../../config';
-import randombytes from 'crypto-browserify';
+import randomBytes from '../utility/randomBytes'
     //! Separate axios configs from twitter requests.
+
+function generate_nonce(){
+    //nonce = randombytes(16).toString('base64');
+    nonce = randomBytes(16, false).toString('base64');
+    return nonce;
+}
 
 export function init(cuskey, seckey){
     axios.defaults.baseURL = 'https://api.twitter.com';
     axios.defaults.headers.post['Accept-Encoding'] = 'gzip';
-
+    generate_nonce();
 }
 
 export function getTokeno2(){
@@ -30,10 +36,7 @@ export function getTokeno2(){
     }
     )
 }
-function generate_nonce(){
-    nonce = randombytes(16).toString('base64');
-    return nonce;
-}
+
 function create_signature(){
     //TODO: create signature out of parameter values. 
 

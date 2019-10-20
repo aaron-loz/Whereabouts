5bc656ef4defdba99e321cd0f078b689383a072c
Aaron Lopez <dazdndcunfusd@gmail.com>
2019-10-20 11:50:27 -0400

    flask server initialization and initial integrations

--- whereabouts-app/src/components/reactComponents/TwitterLogin.js
+++ whereabouts-app/src/components/reactComponents/TwitterLogin.js
@@ -1,38 +1,48 @@
-import {init, getTokeno2,test_search} from '../../config/axiosConfigs';
+import {init, getTokeno2, get_friends} from '../../config/axiosConfigs';
 import React from 'react';
 import { Text, View, Button } from 'react-native';
+import { TextInput } from 'react-native-paper';
+
+var httpBridge = require('react-native-http-bridge');
 
 export default class TwitterLogin extends React.Component {
+
     async componentDidMount(){
+          // initalize the server (now accessible via localhost:1234)
         await init();
         this.twitter =  await getTokeno2();
         this.state.twittoken = await this.twitter.access_token;
     }
-    async testSearch(oauthtoken){
-        t = await(test_search(oauthtoken));
-        this.state.twittwits = t;
-        this.setState(previousState =>({
-            twittwits : previousState.twittwits
+
+    //implement geocodes requests to testSearch
+    async testSearch(geocodes){
+
+    }
+
+    async searchTweets(twitname){
+        this.state.twitname = twitname
+        this.state.following = JSON.stringify(await get_friends(twitname))
+        console.log("following:\n")
+        console.log(this.state.following)
+
+        this.setState(previousState => ({
+            following : previousState.following
         }))
     }
-    async searchTweets(lat, long){
-    }
-    
-    async SSOlogin(oauthtoken){
-    //! TODO: Finish setting up axios nonces.
-        /*console.log(oauthtoken);
-        t = await twitsignin(oauthtoken)
-        console.log(t)*/
-    }
-    state = {twitdetails: ''}
+    state = {twitdetails: '',
+            twitname : '',
+            following : ''}
 
     render() {
         return(
         <View style= {styles.container}>
-            <Text>{this.state.twittwits}</Text>
-            <Button
-            title="Twitter Login Button" onPress={() => this.testSearch(this.state.twittoken)}
+            <Text>Enter your twitter name to continue!</Text>
+            <TextInput
+              style= {{height: 60}}
+              autoCompleteType = 'username'
+              onSubmitEditing={text => this.searchTweets(text)}
             />
+            <Text>{this.state.following}</Text>
         </View>
         )
     }

9720646cb98c4a6e42d7fe521ded393c1246e1d3
Liudmila Zyrianova <liudmila.zyrianova@gmail.com>
2019-10-26 16:04:43 -0400

    query real friends

--- whereabouts-app/src/components/firebase/firebaseApi.js
+++ whereabouts-app/src/components/firebase/firebaseApi.js
@@ -3,18 +3,22 @@ import {  Alert } from 'react-native';
 import { db } from '../../config/firebaseConfig';
 
 //item - string containing user_id
+//Returns snapshot of accountIds
+export function getAccountIdsTable() {
+    return db.ref('/accountIds').once('value', snapshot => {});
+}
+
+//item - string containing user_id
 //Returns true if element is in firebase
-export async function checkHasAccountId(item) {
-    await db.ref('/accountIds').once('value', snapshot => {
-        let data = snapshot.val();
-        let allAccounts = Object.values(data);
-        allAccounts.forEach(elem => {
-          if (elem.id === item) {
-            return true;
-          }
-        })
-        return false;
-    });
+export function checkHasAccountId(snapshot, item) {
+    let data = snapshot.val();
+    let allAccounts = Object.values(data);
+    for (let i=0; i<allAccounts.length; i++) {
+      if (allAccounts[i].id === item) {
+        return true;
+      }
+    }
+    return false;
 }
 //item - string containing user_id
 //Pushes item to firebase
@@ -24,22 +28,25 @@ export function addAccountIds(item){
     });
 }
 
+
+export function getFriendsTable() {
+  return db.ref('/friends').once('value', snapshot => {});
+}
+
 //item - Object {
 //     "id_str": "111",
 //     "screen_name": "Name1",
 //   },
 //Returns true if pair of userIdp and item.id_str is in firebase
-export async function checkHasUserIdAndFriendId(userIdp, item) {
-    await db.ref('/friends/').on('value', snapshot => {
-        let data = snapshot.val();
-        let allFriends = Object.values(data);
-        allFriends.forEach(elem => {
-          if (elem.userId == userIdp && elem.friendId == item.id_str) {
-            return true;
-          }
-        })
-        return false;
-    });
+export function checkHasUserIdAndFriendId(snapshot, userIdp, item) {
+    let data = snapshot.val();
+    let allFriends = Object.values(data);
+    for (let i=0; i<allFriends.length; i++) {
+      if (allFriends[i].userId == userIdp && allFriends[i].friendId == item.id_str) {
+        return true;
+      }
+    }
+    return false;
 }
 //item - Object {
 //     "id_str": "111",
